6de02060eb6a967b178569d6c02f677afea35d13
Youngwan Cho <developer119korea@gmail.com>
2021-12-30 01:58:12 +0900

    구글과 깃헙 계정을 사용하여 로그인기능 추가

---
+++ src/service/firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase/compat/app';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export default firebaseApp;
