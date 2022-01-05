cdea918d00c5fbab4549462356e8ff7c1d1c6b8c
Youngwan Cho <developer119korea@gmail.com>
2022-01-05 23:22:09 +0900

    에디터와 프리뷰 레이아웃 추가

---
+++ src/components/preview/preview.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './preview.module.css';
+
+const Preview = (props) => (
+  <section className={styles.peview}>
+    <h1 className={styles.title}>Preview</h1>
+  </section>
+);
+
+export default Preview;
