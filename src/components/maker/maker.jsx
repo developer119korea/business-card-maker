99919ba431dd3ce916167d16d93f11dc24937149
Youngwan Cho <developer119korea@gmail.com>
2022-01-23 18:03:19 +0900

    에디터와 프리뷰화면에 카드 목록을 보여준다

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Editor from '../editor/editor';
 import Footer from '../footer/footer';
@@ -7,6 +7,42 @@ import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: 1,
+      name: '홍길동',
+      company: 'Samsung',
+      theme: 'dark',
+      title: 'Software Engineer',
+      email: '홍길동@korea.com',
+      message: '내돈내산',
+      fileName: "얍",
+      fileURL: null
+    },
+    {
+      id: 2,
+      name: '김철수',
+      company: 'LG',
+      theme: 'light',
+      title: 'Software Engineer',
+      email: '홍길동@korea.com',
+      message: '내돈내산',
+      fileName: "얍",
+      fileURL: null
+    },
+    {
+      id: 3,
+      name: '박영희',
+      company: '현대',
+      theme: 'colorful',
+      title: 'Software Engineer',
+      email: '홍길동@korea.com',
+      message: '내돈내산',
+      fileName: "얍",
+      fileURL: "fjkdls"
+    }
+  ]);
+
   const navigate = useNavigate();
   const onLogout = () => {
     authService.logout();
@@ -24,8 +60,8 @@ const Maker = ({ authService }) => {
     <section className={styles.maker} >
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} />
+        <Preview cards={cards} />
       </div>
       <Footer />
     </section>
--- src/components/preview/preview.jsx
+++ src/components/preview/preview.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import Card from '../card/card';
 import styles from './preview.module.css';
 
-const Preview = (props) => (
-  <section className={styles.peview}>
-    <h1 className={styles.title}>Preview</h1>
-  </section>
-);
+const Preview = ({ cards }) => {
+  return (
+    <section className={styles.preview}>
+      <h1 className={styles.title}>Preview</h1>
+      <ul className={styles.cards}>
+        {
+          cards.map((card, index) => (
+            <Card key={index} card={card} />
+          ))
+        }
+      </ul>
+    </section>
+  );
+}
 
 export default Preview;
