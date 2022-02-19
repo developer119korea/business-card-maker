bf8acf63ac4774014c9b57316eff92255aec17ec
Youngwan Cho <developer119korea@gmail.com>
2022-02-19 20:19:19 +0900

    프로필 사진을 추가할수 있다

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -3,15 +3,15 @@ import CardAddForm from '../card_add_form/card_add_form';
 import CardEditForm from '../card_edit_form/card_edit_form';
 import styles from './editor.module.css';
 
-const Editor = ({ cards, addCard, updateCard, deleteCard }) => (
+const Editor = ({ FileInput, cards, addCard, updateCard, deleteCard }) => (
   <section className={styles.editor}>
     <h1 className={styles.title} >Card Maker</h1>
     {
       Object.keys(cards).map((key) => (
-        <CardEditForm key={key} card={cards[key]} updateCard={updateCard} deleteCard={deleteCard} />
+        <CardEditForm FileInput={FileInput} key={key} card={cards[key]} updateCard={updateCard} deleteCard={deleteCard} />
       ))
     }
-    <CardAddForm onAddCard={addCard} />
+    <CardAddForm FileInput={FileInput} onAddCard={addCard} />
   </section>
 );
 
---
+++ src/service/image_uploader.js
@@ -0,0 +1,17 @@
+class ImageUploader {
+  async upload(file) {
+    const data = new FormData();
+    data.append('file', file);
+    data.append('upload_preset', process.env.REACT_APP_UPLOAD_PRESET_NAME);
+    const res = await fetch(
+      `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUD_NAME}/upload`,
+      {
+        method: 'POST',
+        body: data,
+      }
+    );
+    return await res.json();
+  }
+}
+
+export default ImageUploader;
